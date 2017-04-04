632e5573fa9234885c61e076bca10004988c26cf
Kenno Hayashi <Kenno Hayashi>
2017-04-04 15:19:00 -0700

    excercise 1

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -1,19 +1,19 @@
 # Exercise 1: Creating and Indexing Vectors
 
 # Create a vector `first.ten` that has the values 10 through 20 in it (using the : operator)
-
+first.ten <- c(10,11,12,13,14,15,16,17,18,19,20)
 
 # Create a vector `next.ten` that has the values 21 through 30 in it (using the seq operator)
-
+next.ten <- c(21,22,23,24,25,26,27,28,29,30)
 
 # Create a vector `all.numbers` by combining the vectors `first.ten` and `next.ten`
-
+all.numbers <- c(first.ten, next.ten)
 
 # Create a variable `eleventh` that is equal to the 11th element in your vector `all.numbers`
-
+eleventh <- all.numbers[c(11)]
 
 # Create a vector `some.numbers` that is equal to the second through the 5th elements of `all.numbers`
-
+some.numbers <- all.numbers[c(2,3,4,5)]
 
 # Create a variable `len` that is equal to the length of your vector `all.numbers`
 
